50e542c649e52b8e59bf231319a39cb39f43122d
Jay <misterhj28@gmail.com>
2026-07-02 14:08:24 +0900

    feat: 로또번호 출력기로 소스 변경

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-print("Hello, World!")
+import random
 
-print("more")
 
-print("more2")
+def generate_lotto_numbers():
+    numbers = random.sample(range(1, 46), 6)
+    numbers.sort()
+    return numbers
 
-print("more3")
+
+if __name__ == "__main__":
+    lotto_numbers = generate_lotto_numbers()
+    print("로또 번호:", lotto_numbers)
